f8eaca65a76986291a2f50cd2ebbbd7852cb3d30
wyo-kaitlyn <wyokaitlyn@gmail.com>
2023-12-11 16:55:27 -0700

    code

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
-function augmentingPath(graph, start, end) {
+function augmentingPath(graph, start, end, path = []) {
+    // adds the start to the path
+    path.push(start);
+
+    // sees if the start is equal to the end, and returns the path
+    if (start == end) {
+        return path;
+    }
+    // iterates through the current node
+    for (let i in graph[start]) {
+        // sees if the node has already been visited
+        if (!path.includes(i)) {
+            // recursively calls to go to the nest node
+            let newPath = augmentingPath(graph, i, end, path);
+            // returns the path if it's found
+            if (newPath.length > 0) { return newPath; }
+            // if it's not, resets the path and adds the start node
+            else {
+                path = [];
+                path.push(start);
+            }
+        }
+    }
+
+    // returns empty if there is no path
     return [];
 }
